3a41fdf3b96348cb14c012ea0bc8b2a072391b4f
Colum Bennett <colum.bennett@feedhenry.com>
2014-07-13 19:12:13 +0100

    adding gitignore, script dir

---
+++ README.md
@@ -0,0 +1,6 @@
+# Workflow shell scripts
+
+Repo is to become central location for all shell scripts used in development
+process. Aim is to create a central location for storing and developing scripts
+to automate and enhance development workflow on the FeedHenry 3 platform.
+
---
+++ scripts/deploy-android.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#
+############################################################
+#
+# .deploy-android.sh
+#
+# This script is automate deploying a Cordova Andoird app
+# on device by removing existing apk using adb, building a 
+# new apk using cordova cli, installing the apk on device 
+# and finally launching the app.
+#
+############################################################
+
